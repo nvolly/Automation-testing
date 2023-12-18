3c50898fd8e9d0f7ab408bcbdc8659ebb887e855
ValentinDragomir94 <dragomirvalentin1994@yahoo.com>
2023-12-18 23:40:33 +0200

    new changes

---
+++ Selenium tests/Svetlana youtube selenium training/Ebay test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver_service = Service(executable_path='D:\python\Automation\Selenium tests\chromedriver.exe')
+driver = webdriver.Chrome(options=options, service=driver_service)
+driver.implicitly_wait(5)
+
+driver.get("https://www.ebay.com/")
+
+
+search = driver.find_element(By.CLASS_NAME, 'gh-tb ui-autocomplete-input')
+search.send_keys('dress', Keys.ENTER)
